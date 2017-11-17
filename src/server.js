ac1d0b58d00d1a7ca1a0dcedfae050de0ea0f6dc
root <root@Opus-VM.vmware.loc>
2017-11-17 10:54:22 +0530

    Add server.js with ssl changes

--- src/server.js
+++ src/server.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var privateKey  = fs.readFileSync('/etc/ssl/vegabot-certs/server.key', 'utf8');
+var certificate = fs.readFileSync('/etc/ssl/vegabot-certs/vegabot_opusconsulting_com.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
 var router = require('./router.js')(app);
 
-var server = app.listen(8082, function () {
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port);
-
-})
+httpServer.listen(8082);
+httpsServer.listen(443);
